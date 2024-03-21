346836d352a0b273b9d61f49528fdc1460c3bce8
Reli <naitvenefy@gmail.com>
2024-03-20 22:53:40 -0600

    identify if playing song is current song

--- src/components/CardPlayButton.tsx
+++ src/components/CardPlayButton.tsx
@@ -11,15 +11,30 @@ const CardPlayButton: React.FC<CardPlayButtonProps> = ({ id }) => {
     usePlayerStore((state) => state);
 
   const handleClick = () => {
+    setCurrentMusic({
+      playlist: {
+        id,
+        albumId: 1,
+        title:"My Playlist",
+        color: { accent: "#da2735", dark: "#7f1d1d" },
+        cover:"https://picsum.photos/id/237/200/300",
+        artists:["Your Name"],
+      },
+      song: null,
+      songs: [],
+    });
+
     setIsPlaying(!isPlaying);
   };
+
+  const isPlayingPlaylist = isPlaying && currentMusic.playlist?.id === id;
 
   return (
     <button
       onClick={handleClick}
       className="card-play-button rounded-full bg-green-500 p-2"
     >
-      {isPlaying ? (
+      {isPlayingPlaylist ? (
         <Pause className="text-black" />
       ) : (
         <Play className="text-black" />
