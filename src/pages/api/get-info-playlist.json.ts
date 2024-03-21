c0df58f62546bfa7e644b81a07e9fb737e5961b4
Reli <naitvenefy@gmail.com>
2024-03-21 00:36:19 -0600

    small refactor on get playlists

--- src/pages/api/get-info-playlist.json.ts
+++ src/pages/api/get-info-playlist.json.ts
@@ -1,23 +1,13 @@
 import { allPlaylists, songs as allSongs } from "@/lib/data";
 
-export async function GET({ params, request }: { params: any; request: any }) {
-  // get the id from the url search params
-  const { url } = request;
-  const urlObject = new URL(url);
-  const id = urlObject.searchParams.get("id");
+export async function GET({ request }: { request: Request }) {
+  const url = new URL(request.url);
+  const playlistId = url.searchParams.get("id");
 
-  const playlist = allPlaylists.find((playlist) => playlist.id === id);
-  const songs = allSongs.filter((song) => song.albumId === playlist?.albumId);
+  const playlist = allPlaylists.find(({ id }) => id === playlistId);
+  const songs = allSongs.filter(({ albumId }) => albumId === playlist?.albumId);
 
-  return new Response(
-    JSON.stringify({
-      playlist,
-      songs,
-    }),
-    {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  );
+  return new Response(JSON.stringify({ playlist, songs }), {
+    headers: { "Content-Type": "application/json" },
+  });
 }
