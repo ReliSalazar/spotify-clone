0e87297469d01abd4ce84478e6188d4cded876c1
Reli <naitvenefy@gmail.com>
2024-03-21 18:45:49 -0600

    play from playlist view

--- src/components/CardPlayButton.tsx
+++ src/components/CardPlayButton.tsx
@@ -4,9 +4,13 @@ import { Pause, Play } from "./Player.tsx";
 
 interface CardPlayButtonProps {
   id: string;
+  size?: string;
 }
 
-const CardPlayButton: React.FC<CardPlayButtonProps> = ({ id }) => {
+const CardPlayButton: React.FC<CardPlayButtonProps> = ({
+  id,
+  size = "small",
+}) => {
   const { isPlaying, currentMusic, setIsPlaying, setCurrentMusic } =
     usePlayerStore((state) => state);
 
@@ -25,15 +29,17 @@ const CardPlayButton: React.FC<CardPlayButtonProps> = ({ id }) => {
     setCurrentMusic({ songs, playlist, song: songs[0] });
   };
 
+  const iconClassName = size === "small" ? "w-4 h-4" : "w-5 h-5";
+
   return (
     <button
       onClick={handleClick}
-      className="card-play-button rounded-full bg-green-500 p-2"
+      className="card-play-button rounded-full bg-green-500 p-4 hover:scale-105 transition hover:bg-green-400"
     >
       {isPlayingPlaylist ? (
-        <Pause className="text-black" />
+        <Pause className={iconClassName} />
       ) : (
-        <Play className="text-black" />
+        <Play className={iconClassName} />
       )}
     </button>
   );
